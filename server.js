6351d063ab4c0022b9cc8ee79a75ca98997895a9
Facundo Farias <facundo.farias@gmail.com>
2015-01-04 16:35:45 +0100

    2015 updates

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8888);
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
